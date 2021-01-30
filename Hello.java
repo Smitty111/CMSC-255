7dc9faf87b0609e6659131f6cff0b5974fdfc76c
Smitty111 <78281005+Smitty111@users.noreply.github.com>
2021-01-30 17:09:53 -0500

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,16 @@
+/************************************************************
+Hello.java
+Simon Smits
+01/27/21
+255 901
+*************************************************************/
+public class Hello{
+	public static void main(String[] args){
+		System.out.println("<Simon Smits>");
+		System.out.println("<Hello.Java>");
+		System.out.println("<Checking to see if Java will run>");
+		System.out.println("<01/27/21>");
+		System.out.println("<255 901>");
+		System.out.println();
+	}
+}
